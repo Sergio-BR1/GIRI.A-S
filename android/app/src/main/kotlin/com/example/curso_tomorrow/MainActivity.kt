d020a2101317c208dbebb9a886842a278d3da48b
Sérgio Brito <mariosbps@ufba.br>
2024-06-18 16:26:00 -0300

    Upando arquivos

---
+++ android/app/src/main/kotlin/com/example/curso_tomorrow/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.curso_tomorrow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'tela_quiz.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TelaQuiz(),
+    ),
+  );
+}
